3444b7a4b4c3815698f425c6c128830fdeda1211
Zach Moazeni <zach.moazeni@gmail.com>
2011-03-06 20:35:03 -0500

    Adding Rails codebase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayingWithSunspot::Application.config.secret_token = '8cc239ab787a1c065089aac2f717818ab517d03124c72b3f2062558b1fe6b18e7e68fc90c8fd712d3e703d25ec3c7f3c589660c151de59057d3b5b0ea4f881da'
